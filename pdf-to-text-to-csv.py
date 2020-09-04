11498a72605ec9a18b8a9397a0acb96327f489c4
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-09-04 21:42:10 +0100

    Update pdf-to-text-to-csv.py

--- pdf-to-text-to-csv.py
+++ pdf-to-text-to-csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 with pdfplumber.open('eni.pdf') as pdf:
-    page = pdf.pages[3]
+    page = pdf.pages[4]
     text = page.extract_text()
     print(text)
     
@@ -11,6 +11,6 @@ file1 = open("myfile.txt","w")
 file1.writelines(text)
 file1.close() #to change file access modes 
 
-data = pd.read_csv('myfile.txt', encoding= 'unicode_escape', header=None, error_bad_lines=False)
+data = pd.read_csv('myfile.txt', encoding= 'unicode_escape', header=None, sep='delimiter', engine='python')
 data.to_excel('myfile.xlsx')
 data
