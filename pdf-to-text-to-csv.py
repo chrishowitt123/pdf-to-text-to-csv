7742448a5cc2dee74530efb2fb001cf25f65dfa9
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-09-04 22:26:02 +0100

    Update pdf-to-text-to-csv.py

--- pdf-to-text-to-csv.py
+++ pdf-to-text-to-csv.py
@@ -1,11 +1,11 @@
 import pdfplumber
 import pandas as pd
 
-filename = 'eni.pdf'
+filename = 'annualfinancialsample.pdf'
 fn = filename.split('.')[0] #seperate filename from extention for f-string output files
 
 with pdfplumber.open(filename) as pdf: #extract text from desired page
-    page = pdf.pages[4]
+    page = pdf.pages[0]
     text = page.extract_text()
     print(text)
     
@@ -13,6 +13,6 @@ file1 = open(f"{fn}.txt","w") #output pdf text as .txt
 file1.writelines(text)
 file1.close() 
 
-data = pd.read_csv('myfile.txt', encoding= 'unicode_escape', header=None, sep='delimiter', engine='python') #read text file as csv and output
+data = pd.read_csv(f"{fn}.txt", encoding= 'unicode_escape', header=None, sep='delimiter', engine='python') #read text file as csv and output
 data.to_excel(f"{fn}.xlsx")
 data
