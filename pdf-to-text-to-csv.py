14e86cb06000c2bf9bcd0c27dbfba0e755a9176f
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-09-04 21:56:50 +0100

    Update pdf-to-text-to-csv.py

--- pdf-to-text-to-csv.py
+++ pdf-to-text-to-csv.py
@@ -2,17 +2,17 @@ import pdfplumber
 import pandas as pd
 
 filename = 'eni.pdf'
-fn = filename.split('.')[0] #seperate filename from extention for filenaming later
+fn = filename.split('.')[0] #seperate filename from extention for f-string output files
 
-with pdfplumber.open(filename) as pdf:
+with pdfplumber.open(filename) as pdf: #extract text from desired page
     page = pdf.pages[4]
     text = page.extract_text()
     print(text)
     
-file1 = open(f"{fn}.txt","w") 
+file1 = open(f"{fn}.txt","w") #output pdf text as .txt
 file1.writelines(text)
-file1.close() #to change file access modes 
+file1.close() 
 
-data = pd.read_csv('myfile.txt', encoding= 'unicode_escape', header=None, sep='delimiter', engine='python')
+data = pd.read_csv('myfile.txt', encoding= 'unicode_escape', header=None, sep='delimiter', engine='python') #read text file as csv and output
 data.to_excel(f"{fn}.xlsx")
 data
